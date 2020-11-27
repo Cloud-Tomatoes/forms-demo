b04d400fcfb41dd5d03897ba26c27fb6372e953d
Gabriela <gabilarreglemail@gmail.com>
2020-11-27 19:58:44 +0100

    install ember-bootstrap, use scss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     sassOptions: {
-      extension: 'sass'
+      extension: 'scss'
+    },
+
+    'ember-bootstrap': {
+      bootstrapVersion: 4,
+      importBootstrapCSS: false
     }
   });
 
