a3e01e8cd915efa6b5a0cc6363ee6f46e69f5593
Gabriela <gabilarreglemail@gmail.com>
2020-11-27 19:59:29 +0100

    update modal-dialog cmp, use ember-bootstrap

--- app/components/modal-dialog.js
+++ app/components/modal-dialog.js
@@ -1,23 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import jQuery from 'jquery'
+import { tryInvoke } from '@ember/utils';
 
 export default class ModalDialogComponent extends Component {
 
 
-  // Lifecycle Hooks
-
-  willDestroy(){
-    this.elementModal.modal('hide').off();
-  }
-
-
   // Actions
 
-  @action openModal(element) {
-    const onClose = this.args.onClose;
-    this.elementModal = jQuery(element);
-    this.elementModal.modal().on('hidden.bs.modal', onClose);
+  @action
+  onHiddenModal() {
+    return tryInvoke(this.args, "onClose");
+  }
+
+  @action
+  submit(element) {
   }
 
 };
