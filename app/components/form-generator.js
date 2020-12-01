7a5749d3a5b95e59514e30fc7c819cff8896886f
Gabriela <gabilarreglemail@gmail.com>
2020-12-01 19:07:55 +0100

    form-generator: create changesets per group (+)
    - fix validations object
    - rm groupsList

--- app/components/form-generator.js
+++ app/components/form-generator.js
@@ -2,34 +2,63 @@ import Component from '@glimmer/component';
 import { Changeset } from 'ember-changeset';
 import { tryInvoke } from '@ember/utils';
 import { action } from '@ember/object';
+import lookupValidator from 'ember-changeset-validations';
+import * as Validators from 'ember-changeset-validations/validators';
 
 export default class FormGeneratorComponent extends Component {
 
-  get bluePrintChangeset() {
-    const blueprint = this.args.blueprint[0];
-    return Changeset(blueprint);
-  }
+
+  // Getteres
 
   get groups() {
-    const blueprint = this.bluePrintChangeset;
-    const properties = blueprint.attributes.properties;
+    const blueprint = this.args.blueprint[0];
+    const { properties } = blueprint.attributes;
     let groups = {};
     for (let prop in properties) {
       const group = properties[prop].ui.group;
       if (!groups[group])
-        groups[group] = []
-      groups[group].push(properties[prop])
+        groups[group] = {}
+      groups[group][prop] = properties[prop];
     }
     return groups;
   }
 
-  get groupsList() {
+  get changesets() {
     const groups = this.groups;
-    return Object.keys(groups);
+    let changesets = [];
+    for (let group in groups) {
+      const validations = this._getValidations(groups[group]);
+      const groupChangeset = Changeset(groups[group], lookupValidator(validations), validations);
+      changesets.push(groupChangeset)
+    }
+    return changesets;
   }
 
-  @action onSubmit() {
+
+  // Actions
+
+  @action
+  onSubmit() {
     return tryInvoke(this.args, 'onSubmit');
+  }
+
+
+  // Private Methods
+
+  _getValidations(group) {
+    let validation = {};
+    for (let name in group) {
+      const validationBlueprint = group[name].validation;
+      if (validationBlueprint) {
+        validation[name] = { value: [] };
+        for (let val in validationBlueprint) {
+          const validatorName = val.split("/").slice(-1);
+          const options = validationBlueprint[val];
+          validation[name]["value"].push(Validators[validatorName](options))
+        }
+      }
+    }
+    return validation;
   }
 
 }
