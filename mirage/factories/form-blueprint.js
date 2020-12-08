5ad82ff57a762396e7f4c5e245103de6f1660e3d
Gabriela <gabilarreglemail@gmail.com>
2020-12-08 23:35:32 +0100

    Mirage setup, mock form-blueprint

---
+++ mirage/factories/form-blueprint.js
@@ -0,0 +1,8 @@
+import { Factory } from 'ember-cli-mirage';
+import blueprintProperties from '../utils/form-blueprint';
+
+export default Factory.extend({
+
+  properties: blueprintProperties
+
+});
---
+++ mirage/utils/form-blueprint.js
@@ -0,0 +1,89 @@
+export default {
+  "name": {
+      "type": "text",
+      "ui": {
+        "label": "first name",
+        "group": "data",
+        "position": 1,
+        "placeholder": "Your name here",
+      },
+      "validation": {
+          "Neos.Neos\/Validation\/validatePresence": {
+            "presence": true,
+            "ignoreBlank": true
+          },
+          "Neos.Neos\/Validation\/validateLength": {
+            "max": 50
+          }
+      }
+  },
+  "lastName": {
+      "type": "text",
+      "ui": {
+        "label": "last name",
+        "group": "data",
+        "position": 2,
+        "placeholder": "Your last name here",
+      },
+      "validation": {
+          "Neos.Neos\/Validation\/validatePresence": {
+            "presence": true,
+            "ignoreBlank": true
+          }
+      }
+  },
+  "age": {
+      "type": "number",
+      "value": 1300,
+      "ui": {
+        "label": "Age",
+        "group": "data",
+        "position": 3,
+        "placeholder": "Your age here"
+      }
+  },
+  "address": {
+      "type": "text",
+      "ui": {
+        "label": "Street",
+        "group": "location"
+      }
+  },
+  "city": {
+      "type": "text",
+      "ui": {
+        "label": "City",
+        "group": "location"
+      }
+  },
+  "EU": {
+      "type": "checkbox",
+      "ui": {
+        "label": "Inside EU",
+        "group": "location"
+      }
+  },
+  "color": {
+      "type": "text",
+      "value": "pink",
+      "ui": {
+        "label": "Favourite color",
+        "group": "misc",
+        "placeholder": "Favourite color"
+      },
+      "validation": {
+          "Neos.Neos\/Validation\/validatePresence": {
+            "presence": true,
+            "ignoreBlank": true
+          }
+      }
+  },
+  "description": {
+      "type": "textarea",
+      "ui": {
+        "label": "Description",
+        "group": "misc",
+        "placeholder": "Color description"
+      }
+  }
+}
